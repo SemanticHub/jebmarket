65b08d5cf67b39e44a84d1879b93dd30f2766efe
Syed Ekram Uddin Emon <ekram.syed@gmail.com>
2013-08-19 19:25:50 +0000

    resolve index.php conflicts and commit other changes

---
+++ resources/protected/views/settings/_form.php
@@ -0,0 +1,70 @@
+<?php
+/* @var $this SettingsController */
+/* @var $model Settings */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'settings-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'description'); ?>
+		<?php echo $form->textField($model,'description',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'description'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'options'); ?>
+		<?php echo $form->textField($model,'options',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'options'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'value'); ?>
+		<?php echo $form->textField($model,'value',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'value'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'type'); ?>
+		<?php echo $form->textField($model,'type',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'type'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'validation'); ?>
+		<?php echo $form->textField($model,'validation',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'validation'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'tag'); ?>
+		<?php echo $form->textField($model,'tag',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'tag'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
