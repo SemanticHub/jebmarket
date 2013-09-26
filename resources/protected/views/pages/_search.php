31da3be1d677fd9bf12d767c55a96f80b6601357
Syed Ekram Uddin Emon <ekram.syed@gmail.com>
2013-09-26 22:43:45 +0000

    recovered default views and files from old revision

---
+++ resources/protected/views/pages/_search.php
@@ -0,0 +1,60 @@
+<?php
+/* @var $this PagesController */
+/* @var $model Pages */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'active'); ?>
+		<?php echo $form->textField($model,'active',array('size'=>1,'maxlength'=>1)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'content'); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'slug'); ?>
+		<?php echo $form->textField($model,'slug',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'meta_title'); ?>
+		<?php echo $form->textField($model,'meta_title',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'meta_desc'); ?>
+		<?php echo $form->textArea($model,'meta_desc',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'meta_keywords'); ?>
+		<?php echo $form->textField($model,'meta_keywords'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
---
+++ resources/protected/views/settings/_view.php
@@ -0,0 +1,24 @@
+<?php
+/* @var $this SettingsController */
+/* @var $data Settings */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('value')); ?>:</b>
+	<?php echo CHtml::encode($data->value); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tag')); ?>:</b>
+	<?php echo CHtml::encode($data->tag); ?>	<br />
+
+</div>
