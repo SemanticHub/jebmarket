9ef4067e54ca8fccc70c5ab6b0932d892574075c
ekram <ekram@222ee1a1-3961-4696-891a-0e3d05bc86c3>
2013-09-30 20:10:48 +0000

    Authorization Role and Rights Component

--- trunk/resources/protected/config/main.php
+++ trunk/resources/protected/config/main.php
@@ -1,61 +1,72 @@
 <?php
 // This is the main Web application configuration.
 return array(
-    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'JebMarket',
-    'sourceLanguage'=> '00',
-    'language'=> 'en',    
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'JebMarket',
+    'sourceLanguage' => '00',
+    'language' => 'en',
     # default theme
-    'theme'=> 'jebmarket',
+    'theme' => 'jebmarket',
     # preloading 'log' component
-    'preload'=>array('log'),
+    'preload' => array('log'),
     # autoloading model and component classes
-    'import'=>array(
+    'import' => array(
         'application.models.*',
         'application.components.*',
+        'application.modules.rights.*',
+        'application.modules.rights.components.*'
     ),
     # application modules
-    'modules'=>array(
+    'modules' => array(
         # Gii tool
-        'gii'=>array(
-                'class'=>'system.gii.GiiModule',
-                'password'=>'mustbedisableonproduction',
-                // If removed, Gii defaults to localhost only. Edit carefully to taste.
-                'ipFilters'=>array('*','::1'),
+        'gii' => array(
+            'class' => 'system.gii.GiiModule',
+            'password' => 'mustbedisableonproduction',
+            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+            'ipFilters' => array('*', '::1'),
         ),
+        'rights'=>array(
+            'install'=> true,
+            'superuserName' => 'Jebdmin',
+            'authenticatedName' => 'Registered',
+            'flashSuccessKey'=>'success',
+            'flashErrorKey'=>'error',
+            'debug'=>true
+        )
     ),
     # application components
-    'components'=>array(
+    'components' => array(
         # Theme Settings
-        'themeManager'=>array(
-            'basePath'=> dirname(__FILE__).'/../themes',                    
+        'themeManager' => array(
+            'basePath' => dirname(__FILE__) . '/../themes',
         ),
         'clientScript' => array(
             'scriptMap' => array(
                 'jquery.min.js' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
             ),
         ),
-        'user'=>array(
+        'user' => array(
             // enable cookie-based authentication
-            'allowAutoLogin'=>true,
-            'class' => 'WebUser',
+            'allowAutoLogin' => true,
+            //'class' => 'WebUser',
+            'class' => 'RWebUser',
         ),
         # enable URLs in path-format
-        'urlManager'=>array(
-            'urlFormat'=>'path',
+        'urlManager' => array(
+            'urlFormat' => 'path',
             'showScriptName' => false,
-            'rules'=>array(
-                    'gii'=>'gii',
-                    'gii/<controller:\w+>'=>'gii/<controller>',
-                    'gii/<controller:\w+>/<action:\w+>'=>'gii/<controller>/<action>',
-                    '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                    '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-                    '<controller:\w+>/<action:\w+>/<view:\w+>'=>'<controller>/<action>',
-                    '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            'rules' => array(
+                'gii' => 'gii',
+                'gii/<controller:\w+>' => 'gii/<controller>',
+                'gii/<controller:\w+>/<action:\w+>' => 'gii/<controller>/<action>',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<view:\w+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ),
         ),
         # MySQL Database Access
-        'db'=>array(
+        'db' => array(
             'connectionString' => 'mysql:host=localhost;dbname=jassifie_yii_jeb',
             'emulatePrepare' => true,
             'username' => 'jassifie_yiijeb',
@@ -64,24 +75,25 @@ return array(
         ),
         # Authorization Manager
         'authManager' => array(
-            'class' => 'CDbAuthManager',
+            //'class' => 'CDbAuthManager',
+            'class' => 'RDbAuthManager',
             'connectionID' => 'db',
             'assignmentTable' => 'jebapp_auth_assignment',
             'itemTable' => 'jebapp_auth_item',
             'itemChildTable' => 'jebapp_auth_item_child',
         ),
-    # Default Error Handler
-        'errorHandler'=>array(
+        # Default Error Handler
+        'errorHandler' => array(
             # use 'site/error' action to display errors
-            'errorAction'=>'site/error',
+            'errorAction' => 'site/error',
         ),
         # Application Log
-        'log'=>array(
-            'class'=>'CLogRouter',
-            'routes'=>array(
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
                 array(
-                    'class'=>'CFileLogRoute',
-                    'levels'=>'error, warning',
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
                 ),
                 # uncomment the following to show log messages on web pages on development.
                 /*array(
@@ -89,30 +101,30 @@ return array(
                 ),*/
             ),
         ),
-),        
+    ),
     # application-level parameters that can be accessed Yii::app()->params['paramName']
-    'params'=>array(
-        'superAdminEmail'=>'ekram.syed@gmail.com',
-        'adminEmail'=>'admin@jebmarket.com',
-        'supportEmail'=>'support@jebmarket.com',
-        'sitemenu'=> array(
-            'site/contact'=>'Contact',
-            'site/login'=>'Login',
-            'user/signup'=>'Signup',
-            'faq/index'=> 'FAQ',
-            'user/profile'=> 'User Profile'
+    'params' => array(
+        'superAdminEmail' => 'ekram.syed@gmail.com',
+        'adminEmail' => 'admin@jebmarket.com',
+        'supportEmail' => 'support@jebmarket.com',
+        'sitemenu' => array(
+            'site/contact' => 'Contact',
+            'site/login' => 'Login',
+            'user/signup' => 'Signup',
+            'faq/index' => 'FAQ',
+            'user/profile' => 'User Profile'
         ),
-        'usermenu'=> array(
-           'account'=> array('label'=>'User', 'linkOptions'=>array('class'=>'list-group-title')),
-           'dashboard' => array('label'=>'Dashboard', 'url'=>array('dashboard')),
-           'profile' => array('label'=>'Profile', 'url'=>array('profile')),
-           'edit' => array('label'=>'Edit Account', 'url'=>array('edit')),
-           'password' => array('label'=>'Change Password', 'url'=>array('password')),
-           'store' => array('label'=>'Store', 'linkOptions'=>array('class'=>'list-group-title')),
-           'products' => array('label'=>'Products', 'url'=>array('products')),
-           'orders' => array('label'=>'Orders', 'url'=>array('orders')),
+        'usermenu' => array(
+            'account' => array('label' => 'User', 'linkOptions' => array('class' => 'list-group-title')),
+            'dashboard' => array('label' => 'Dashboard', 'url' => array('dashboard')),
+            'profile' => array('label' => 'Profile', 'url' => array('profile')),
+            'edit' => array('label' => 'Edit Account', 'url' => array('edit')),
+            'password' => array('label' => 'Change Password', 'url' => array('password')),
+            'store' => array('label' => 'Store', 'linkOptions' => array('class' => 'list-group-title')),
+            'products' => array('label' => 'Products', 'url' => array('products')),
+            'orders' => array('label' => 'Orders', 'url' => array('orders')),
         ),
-        'adminmenu'=> array(                   
+        'adminmenu' => array(
             array('label' => Yii::t('phrase', 'Users'), 'url' => array('/user/admin'), 'visible' => !Yii::app()->user->isGuest),
             array('label' => Yii::t('phrase', 'Sliders'), 'url' => array('/slider/admin'), 'visible' => !Yii::app()->user->isGuest),
             array('label' => Yii::t('phrase', 'Email Templates'), 'url' => array('/emailTemplate/admin'), 'visible' => !Yii::app()->user->isGuest),
@@ -122,7 +134,7 @@ return array(
             array('label' => Yii::t('phrase', 'Settings'), 'url' => array('/settings/admin'), 'visible' => !Yii::app()->user->isGuest),
             array('label' => Yii::t('phrase', 'Logout'), 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
         ),
-        'uploadPath'=>dirname(__FILE__).'/../data/media/upload',
-        'uploadUrl'=>'assets/up/',
+        'uploadPath' => dirname(__FILE__) . '/../data/media/upload',
+        'uploadUrl' => 'assets/up/',
     ),
 );
