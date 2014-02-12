dbfe8ea2edea3d30e90daecdb3879b50f6e9690c
zahangir070 <zahangir070@222ee1a1-3961-4696-891a-0e3d05bc86c3>
2014-02-12 07:44:44 +0000

    Url formating for user detect and fixed menu, page for different user its initial release, if problem then need to fixed

--- trunk/resources/protected/modules/blog/components/BlogComments.php
+++ trunk/resources/protected/modules/blog/components/BlogComments.php
@@ -5,7 +5,12 @@ Yii::import('zii.widgets.CPortlet');
 class BlogComments extends CPortlet {
     public $category;
     public function	renderContent() {
-        $this->category = BlogTerms::model()->findAll(array('condition' => "taxonomy = 'category'"));
+        $user_id_url = Yii::app()->request->getParam('user_id');
+        if(!empty($user_id_url)){
+            $this->category = BlogTerms::model()->findAll(array('condition' => "taxonomy = 'category' AND jebapp_user_id=$user_id_url"));
+        }else{
+            $this->category = BlogTerms::model()->findAll(array('condition' => "taxonomy = 'category'"));
+        }
         $this->render('_commentswidget');
     }
 }
