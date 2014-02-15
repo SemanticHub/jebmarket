d6894bfe128689c4dfceb528d898fa8ff240da17
zahangir070 <zahangir070@222ee1a1-3961-4696-891a-0e3d05bc86c3>
2014-02-15 19:27:12 +0000

    Add blog category, tag searching when click any category or tag

--- trunk/resources/protected/modules/blog/components/BlogComments.php
+++ trunk/resources/protected/modules/blog/components/BlogComments.php
@@ -4,8 +4,10 @@ Yii::import('zii.widgets.CPortlet');
 
 class BlogComments extends CPortlet {
     public $category;
+    public $user;
     public function	renderContent() {
         $user_id_url = Yii::app()->request->getParam('user_id');
+        $this->user = User::model()->findByPk($user_id_url);
         if(!empty($user_id_url)){
             $this->category = BlogTerms::model()->findAll(array('condition' => "taxonomy = 'category' AND jebapp_user_id=$user_id_url"));
         }else{
--- trunk/resources/protected/modules/blog/components/views/_commentswidget.php
+++ trunk/resources/protected/modules/blog/components/views/_commentswidget.php
@@ -5,9 +5,18 @@
         </div>
         <div class="portlet-content">
             <ul class="tag_category">
-                <?php foreach($this->category as $category){ ?>
-                    <li><a href="#" class="label label-success"><kbd><?php echo $category->name; ?></kbd></a></li>
-                <?php } ?>
+                <?php
+                foreach($this->category as $tsqt){
+                    $category = BlogTerms::model()->findAll(array('condition' => "taxonomy = 'category' AND term_id = '$tsqt->term_id'"));
+                    foreach($category as $tst){
+                        if(!empty($this->user)){
+                            echo '<li><a href="'.Yii::app()->baseURL.'/'.$this->user->username.'/blog/category/view/name/'.$tst->slug.'" class="label label-success"><kbd>'.$tst->name.'</kbd></a></li>';
+                        }else{
+                            echo '<li><a href="'.Yii::app()->baseURL.'/blog/category/view/name/'.$tst->slug.'" class="label label-success"><kbd>'.$tst->name.'</kbd></a></li>';
+                        }
+                    }
+                }
+                ?>
             </ul>
         </div>
     </div>
