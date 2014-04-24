6ba463ec109a72a61a4b8b1fbd10558013509f4f
zahangir070 <zahangir070@222ee1a1-3961-4696-891a-0e3d05bc86c3>
2014-04-24 05:04:06 +0000

    Add ckeditor full version

---
+++ trunk/webapp/themes/jebmarket/comp/ckeditor/plugins/highslide/lang/zh-cn.js
@@ -0,0 +1,6 @@
+﻿//dialog
+FCKLang.HighSlideJS  					= 'Highslide JS图片特效' ;
+FCKLang.DlgHighSlide_SetHsJS  			= '应用所有图片Highslide JS特效：' ;
+FCKLang.DlgHighSlide_SetHsJSBtn  		= '应用图片Highslide JS特效' ;
+
+FCKLang.DlgHighSlide_ApplySuccess  		= '<font style=\"color:red\">所有图片Highslide JS特效应用成功！</font>' ;
