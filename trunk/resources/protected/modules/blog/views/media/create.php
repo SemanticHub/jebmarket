aa12c7b342d61da3b9e29f718db09f674b7496a4
zahangir070 <zahangir070@222ee1a1-3961-4696-891a-0e3d05bc86c3>
2014-06-26 00:19:13 +0000

    fixed website module

--- trunk/resources/protected/modules/blog/views/media/create.php
+++ trunk/resources/protected/modules/blog/views/media/create.php
@@ -1,21 +1,42 @@
+<style>
+    .sidebar, .dash_second_menu{
+        display: none;
+    }
+    .dashboard .dash_content_body {
+        margin-top: 0;
+    }
+    .dashboard .col-md-10 {
+        width: 100%;
+    }
+    .media_view img{
+        height: 65px;
+    }
+    .media_view{
+        height: 65px;
+        width: 100px;
+        margin: 0 7px 30px 0;
+        padding: 0;
+        float: left;
+    }
+    .img-thumbnail:hover{
+        background-color: #BC89EB;
+    }
+    .model_cont{
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        float: left;
+    }
+    .media_view .radio_v{
+        margin: 0 0 0 42px;
+        padding: 0;
+        float: left;
+    }
+</style>
 <?php
 $this->menu = Yii::app()->params['usermenu'];
 $this->menu['media']['active'] = true;
 ?>
-    <div class="row">
-        <div class="col-md-6">
-            <h1 class="page-title">Create Media</h1>
-        </div>
-        <div class="col-md-6">
-            <div class="right_top_menu">
-                <ul class="list-inline">
-                    <li>
-                        <?php echo CHtml::link('Manage Media',array('admin'), array('class'=>'btn btn-success')); ?>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
 <div class="media_upload">
     <?php
     $this->widget('ext.JebUpload.JebUpload',
@@ -32,8 +53,34 @@ $this->menu['media']['active'] = true;
                                   }",
                 'onComplete'=>"js:function(file, response, responseJSON) {
                                     $('.alert-success:last').prepend('<img style=margin:0; src=".Yii::app()->baseUrl."/".Yii::app()->params['uploadPath'].Yii::app()->user->id."/media/'+responseJSON['filename']+'>');
+                                    $.fn.yiiListView.update('listviewsf');
                                 }",
             )
         ));
     ?>
 </div>
+<div class="container">
+    <div class="row">
+        <?php $this->widget('zii.widgets.CListView', array(
+            'dataProvider'=>$dataProvider,
+            'itemView'=>'_view',
+            'id' => 'listviewsf',
+        )); ?>
+    </div>
+    <div class="row">
+        <button type="button" class="btn btn-default dismisss">Close</button>
+        <button type="button" class="btn btn-success postinsert">Add This Image</button>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $(".postinsert").click(function(){
+            $(".cke_dialog input[type='text']:visible:first", window.opener.document).val("<?php echo Yii::app()->getBaseUrl(true)."/".Yii::app()->params["uploadPath"].Yii::app()->user->id."/media/"; ?>"+$('input[name=mediaviewradio]:radio:checked').val());
+            window.close();
+        });
+        $(".dismisss").click(function(){
+            window.close();
+        });
+        $("input[name=mediaviewradio]").attr('checked','checked');
+    })
+</script>
