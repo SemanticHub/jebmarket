3b61267448147d65610e2f8c30f04058a91f1f47
ekram <ekram@222ee1a1-3961-4696-891a-0e3d05bc86c3>
2014-06-29 21:29:21 +0000

    For AWS

--- trunk/resources/protected/components/Controller.php
+++ trunk/resources/protected/components/Controller.php
@@ -59,23 +59,25 @@ class Controller extends RController {
         if( Yii::app()->user->id) {
             $user = User::model()->findByPk(Yii::app()->user->id);
             // if user email not verified
-            if ($user->activationstatus == 0 ) {
-                $from = strtotime($user->joined);
-                $to = strtotime(date("Y-m-d H:i:s"));
-                $diff = $to - $from;
-                $days = round($diff / 60 / 60 / 24);
-                $remainingDays = Yii::app()->params['emailVerificationLimit'] - $days;
-                    if($days < Yii::app()->params['emailVerificationLimit'] ) {
-                        Yii::app()->params['activationStatus'] = array(
-                            'count' => $remainingDays,
-                        );
+            if ($user) {
+                if ($user->activationstatus == 0 ) {
+                    $from = strtotime($user->joined);
+                    $to = strtotime(date("Y-m-d H:i:s"));
+                    $diff = $to - $from;
+                    $days = round($diff / 60 / 60 / 24);
+                    $remainingDays = Yii::app()->params['emailVerificationLimit'] - $days;
+                        if($days < Yii::app()->params['emailVerificationLimit'] ) {
+                            Yii::app()->params['activationStatus'] = array(
+                                'count' => $remainingDays,
+                            );
 
-                    } else {
-                        $user->status = 0;
-                        $user->update();
-                        Yii::app()->user->logout();
-                        Yii::app()->user->setFlash('danger', 'Your account is suspended. You\'ve not yet verify your email address. Please verify your email by clicking on the verification link in the email we sent during registration or, '.CHtml::link("Click Here", array('user/sendemailverification', 'user'=>$user->username),array()).' for resend the verification email');
-                    }
+                        } else {
+                            $user->status = 0;
+                            $user->update();
+                            Yii::app()->user->logout();
+                            Yii::app()->user->setFlash('danger', 'Your account is suspended. You\'ve not yet verify your email address. Please verify your email by clicking on the verification link in the email we sent during registration or, '.CHtml::link("Click Here", array('user/sendemailverification', 'user'=>$user->username),array()).' for resend the verification email');
+                        }
+                }
             }
         }
     }
